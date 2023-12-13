a1ec901824b319b4c926b10c0f6af553f9442584
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2023-12-13 18:32:53 +0000

    yamls for k8s

--- lambda.py
+++ lambda.py
@@ -1,16 +1,11 @@
 from typing import Dict, Any
 
-N_MIN = 12
-
-def findCPUandMemKeys(input: dict):
+def findCPUMemandNetKeys(input: dict):
     cpus_percent = []
-    memory_percent = 0
+
     for key in input.keys():
         if "cpu_percent" in key:
             cpus_percent.append(input[key])
-        
-        if "virtual_memory-percent" in key:
-            memory_percent = input[key]
 
         if "net_io_counters_eth0-bytes_sent1" in key:
             bytes_sent = input[key]
@@ -27,12 +22,12 @@ def findCPUandMemKeys(input: dict):
         if "virtual_memory-total" in key:
             total_mem = input[key]
 
-    return cpus_percent, memory_percent
+    return cpus_percent, bytes_sent, bytes_recv, mem_buff, mem_cached, total_mem
 
 
 def handler(input: dict, context: object) -> Dict[str, Any]:
 
-    cpus_percent, memory_percent, bytes_sent, bytes_recv, mem_buff, mem_cached, total_mem = findCPUandMemKeys(input)
+    cpus_percent, bytes_sent, bytes_recv, mem_buff, mem_cached, total_mem = findCPUMemandNetKeys(input)
     output = {}
 
     if getattr(context.env, "counter", None) is None:
@@ -43,16 +38,14 @@ def handler(input: dict, context: object) -> Dict[str, Any]:
             context.env[label_min] = cpu
             output[label_min] = cpu
 
-        
-
         return output
     
     context.env["counter"] += 1
     counter = context.env["counter"]
 
-    n_min = N_MIN
+    n_min = 60
 
-    if counter < N_MIN:
+    if counter < 60:
         n_min = counter
 
 
@@ -62,5 +55,13 @@ def handler(input: dict, context: object) -> Dict[str, Any]:
         new_mvg_avg_min = (last_mvg_avg_min * (n_min - 1) + cpu) / n_min
         context.env[label_min] = new_mvg_avg_min
         output[label_min] = new_mvg_avg_min
+
+    total_bytes = bytes_sent + bytes_recv
+    outgoing_percent = (bytes_sent / total_bytes) * 100 if total_bytes > 0 else 0
+    output["outgoing_traffic"] = outgoing_percent
+
+    cached_memory = mem_cached + mem_buff
+    caching_percentage = (cached_memory / total_mem) * 100
+    output["memory_caching"] = caching_percentage
 
     return output
