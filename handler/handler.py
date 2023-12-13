39510ff6e2b446c98fccfb4e6c6a5d7142399fa0
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2023-12-13 18:53:45 +0000

    adding the rest of the files

---
+++ handler/handler.py
@@ -0,0 +1,55 @@
+from typing import Dict, Any
+
+def find_cpu_mem_and_net_keys(input_data: dict) -> Any:
+    cpus_percent = [input_data[key] for key in input_data if "cpu_percent" in key]
+    bytes_sent = input_data.get("net_io_counters_eth0-bytes_sent1", 0)
+    bytes_recv = input_data.get("net_io_counters_eth0-bytes_recv1", 0)
+    mem_buff = input_data.get("virtual_memory-buffers", 0)
+    mem_cached = input_data.get("virtual_memory-cached", 0)
+    total_mem = input_data.get("virtual_memory-total", 0)
+
+    return cpus_percent, bytes_sent, bytes_recv, mem_buff, mem_cached, total_mem
+
+def update_cpu_average(cpus_percent: Any, context: object) -> Dict[str, Any]:
+    output = {}
+    
+    if context.env.get("counter") is None:
+        context.env["counter"] = 1
+
+        for idx, cpu in enumerate(cpus_percent):
+            label_min = f"mvg_avg_cpu_{idx}_last_minute"
+            context.env[label_min] = cpu
+            output[label_min] = cpu
+
+        return output
+    
+    context.env["counter"] += 1
+    counter = context.env["counter"]
+
+    n_min = min(60, counter)
+
+    for idx, cpu in enumerate(cpus_percent):
+        label_min = f"mvg_avg_cpu_{idx}_last_minute"
+        last_mvg_avg_min = context.env[label_min]
+        new_mvg_avg_min = (last_mvg_avg_min * (n_min - 1) + cpu) / n_min
+        context.env[label_min] = new_mvg_avg_min
+        output[label_min] = new_mvg_avg_min
+
+    return output
+
+def calculate_and_update_metrics(input_data: dict, context: object) -> Dict[str, Any]:
+    cpus_percent, bytes_sent, bytes_recv, mem_buff, mem_cached, total_mem = find_cpu_mem_and_net_keys(input_data)
+    output = update_cpu_average(cpus_percent, context)
+
+    total_bytes = bytes_sent + bytes_recv
+    outgoing_percent = (bytes_sent / total_bytes) * 100 if total_bytes > 0 else 0
+    output["outgoing_traffic"] = outgoing_percent
+
+    cached_memory = mem_cached + mem_buff
+    caching_percentage = (cached_memory / total_mem) * 100
+    output["memory_caching"] = caching_percentage
+
+    return output
+
+def handler(input_data: dict, context: object) -> Dict[str, Any]:
+    return calculate_and_update_metrics(input_data, context)
---
+++ runtime/runtime.py
@@ -0,0 +1,73 @@
+import datetime
+import json
+import redis
+import os
+import importlib
+import time
+
+user_module = importlib.util.find_spec('usermodule')
+
+if not user_module:
+    print("usermodule.py not found")
+    exit(1)
+
+import usermodule
+
+class Context:
+    def __init__(self, host, port, input_key, output_key):
+        self.host = host
+        self.port = port
+        self.input_key = input_key
+        self.output_key = output_key
+        self.last_execution = None
+        self.env = {}
+        self.update_last_execution_time()
+
+    def set_env(self, env):
+        self.env = env
+
+    def update_last_execution_time(self):
+        tmp = os.path.getmtime("/app/usermodule.py")
+        self.function_getmtime = datetime.datetime.fromtimestamp(tmp).strftime('%Y-%m-%d %H:%M:%S')
+        self.last_execution = datetime.datetime.now()
+
+def main():
+    REDIS_HOST = os.getenv('REDIS_HOST', "localhost")
+    REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+    REDIS_INPUT_KEY = os.getenv('REDIS_INPUT_KEY', None)
+    REDIS_OUTPUT_KEY = os.getenv('REDIS_OUTPUT_KEY', None)
+
+    SLEEP_TIME = 5
+
+    with redis.Redis(host=REDIS_HOST, port=REDIS_PORT, charset="utf-8", decode_responses=True) as redis_client:
+        if not REDIS_OUTPUT_KEY:
+            print("Cannot find REDIS OUTPUT KEY")
+            exit(1)
+
+        context = Context(host=REDIS_HOST, port=REDIS_PORT, input_key=REDIS_INPUT_KEY, output_key=REDIS_OUTPUT_KEY)
+
+        while True:
+            try:
+                input_data = redis_client.get(REDIS_INPUT_KEY)
+                if input_data:
+                    input_data = json.loads(input_data)
+                    handle_redis_input(input_data, context, redis_client)
+
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+            time.sleep(SLEEP_TIME)
+
+def handle_redis_input(input_data, context, redis_client):
+    try:
+        output = usermodule.handler(input_data, context)
+        if context.output_key and output:
+            redis_client.set(context.output_key, json.dumps(output))
+
+        context.update_last_execution_time()
+
+    except Exception as e:
+        print(f"Cannot send the output of handler to Redis: {str(e)}")
+
+if __name__ == '__main__':
+    main()
